6e0d59be14046add3c27488450e3f60ac08fce6c
Ali <ali8465@gmail.com>
2021-04-20 22:27:05 +0430

    top menu bug fixed

--- dist/bundle-built.js
+++ dist/bundle-built.js
@@ -29248,7 +29248,7 @@ define('app/featuresMenu/featuresMenu',["require", "exports", "app/model/service
             let currentMenuLi = currentMenu.is("a") ? currentMenu.parent() : currentMenu;
             let currentMenuparent = currentMenu.is("a") ? currentMenu.parent().parent() : currentMenu.parent();
             let parentHasChild = currentMenu.is("a") ? currentMenu.parent().find("ul").length > 0 : currentMenu.find("ul").length > 0;
-            if (hasChild || (link.parent().attr("side-menu-parent") != currentMenuLi.attr("id") && link.parent().attr("side-menu-parent") != currentMenuLi.attr("side-menu-parent"))) //|| (parent.first().is(link.parent().parent()) && parentHasChild && hasChild)
+            if (hasChild || link.parent().attr("side-menu-parent") == undefined || (link.parent().attr("side-menu-parent") != currentMenuLi.attr("id") && link.parent().attr("side-menu-parent") != currentMenuLi.attr("side-menu-parent"))) //|| (parent.first().is(link.parent().parent()) && parentHasChild && hasChild)
                 $(".features-sub-menu").empty();
             // Set the active item
             $(".active").removeClass("active");
