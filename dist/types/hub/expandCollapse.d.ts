4ab3de1b85fb20db34f5842318df8829f9909018
mohammad_hoseinzadehashraf_geeksltd <mohammad.hoseinzadehashraf@geeks.ltd.uk>
2023-11-17 18:52:22 +0000

    redesign expand-collapse module

--- dist/types/hub/expandCollapse.d.ts
+++ dist/types/hub/expandCollapse.d.ts
@@ -1,17 +1,19 @@
-import { FeaturesMenuFactory } from "./featuresMenu/featuresMenu";
 export default class ExpandCollapse {
     button: JQuery;
+    backdrop: JQuery;
     panel: JQuery;
+    page: JQuery;
+    side: string;
     key: string;
     cookies: any;
-    featuresMenuFactory: FeaturesMenuFactory;
-    constructor(button: JQuery, panelKey: string);
-    static enableExpandCollapse(buttonSelector: string, panelSelector: string): void;
-    isCollapsed(): boolean;
+    constructor(side: string);
+    static enableExpandCollapse(side: string): void;
+    static autoCloseOnMobile(): void;
+    static isMobile(): boolean;
+    isExpanded(): boolean;
     initialize(): void;
     toggle(): void;
     apply(): void;
     applyIcon(): void;
-    syncHubTopMenu(): void;
     syncHubFrame(): void;
 }
